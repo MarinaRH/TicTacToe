2510cca1a0c3cfbb83b308882029ae22202f088c
MarinaRH <l_marina1717@hotmail.com>
2017-11-08 22:37:09 -0500

    Agregando el evento Clear al button

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
 window.onload=function() {
     var board =document.querySelector('.board-js');/*haciendo referencia a un objeto de html que es una tabla*/
-
-                                                   /*solo se modifica una clase*/
+    var btn =document.querySelector('.btn');
+                                                   
 board.addEventListener('click',addX);
-};    /*agregando un evento ,se puede agregar varios eventos*/
-   var centinel= true;
-   function addX(event) {
-    if(centinel){
-        event.target.textContent = 'x'; 
+btn.addEventListener('click',clear);
+};  
+
+var centinel= true;
+function addX(event) {
+    if( centinel){
+        event.target.textContent = 'x';  /* es igual a innerHTML.....se puede agregar etiquetas <strong></strong>*/
     }else 
         event.target.textContent = 'o'; 
      centinel=!centinel;
-    /* es igual a innerHTML.....se puede agregar etiquetas <strong></strong>*/
+}
+
+ /*evento para limpiar el juego*/
+function clear(event) {
+       location.reload();
 }
